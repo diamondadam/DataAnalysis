aa1e8a55ca59343e425723c62f92ec624b8d6ecc
Adam Diamond <diamondadam94@gmail.com>
2020-04-18 20:48:49 -0400

    Update DataAnalysisProgram.py

--- DataAnalysisProgram.py
+++ DataAnalysisProgram.py
@@ -12,7 +12,7 @@ from mysql.connector import errorcode
 from sklearn.neighbors import KNeighborsClassifier
 
 try:
-    cnx = mysql.connector.connect(user='adam', password='grinding', host='10.0.0.27', database="machdata")
+    cnx = mysql.connector.connect(user='user', password='password', host='your_host', database="your_database")
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
         print("ERROR: Wrong Credentials")
@@ -23,30 +23,20 @@ except mysql.connector.Error as err:
         print(err)
 else:
     cursor = cnx.cursor(buffered=True)
-    select_feature = "SELECT feature_one, feature_two, feature_three, feature_four, feature_five, feature_six, feature_seven, feature_eight, feature_nine, feature_ten FROM customToolData"
-    select_feature_w_setup = "SELECT setup_hours, feature_one, feature_two, feature_three, feature_four, feature_five, feature_six, feature_seven, feature_eight, feature_nine, feature_ten FROM customToolData"
+    select_feature = "SELECT feature_one, feature_two, feature_three, feature_four, feature_five, feature_six, feature_seven, feature_eight, feature_nine, feature_ten FROM your_table"
+    select_feature_w_setup = "SELECT setup_hours, feature_one, feature_two, feature_three, feature_four, feature_five, feature_six, feature_seven, feature_eight, feature_nine, feature_ten FROM your_table"
 
-    select_setup = "SELECT setup_hours FROM customToolData"
-    select_labor = "SELECT labor_hours FROM customToolData"
+    select_setup = "SELECT setup_hours FROM your_table"
+    select_labor = "SELECT labor_hours FROM your_table"
 
     featureData = pd.read_sql(select_feature, con=cnx)
     setupData = pd.read_sql(select_setup, con=cnx)
     laborData = pd.read_sql(select_labor, con=cnx)
     setup_with_features = pd.read_sql(select_feature_w_setup, con=cnx)
+    
     print(featureData)
     print(setupData)
     print(laborData)
-    # Encode all of the features as integers using LabelEncoder
-    setup_with_features['feature_one'] = LabelEncoder().fit_transform(setup_with_features.feature_one)
-    setup_with_features['feature_two'] = LabelEncoder().fit_transform(setup_with_features.feature_two)
-    setup_with_features['feature_three'] = LabelEncoder().fit_transform(setup_with_features.feature_three)
-    setup_with_features['feature_four'] = LabelEncoder().fit_transform(setup_with_features.feature_four)
-    setup_with_features['feature_five'] = LabelEncoder().fit_transform(setup_with_features.feature_five)
-    setup_with_features['feature_six'] = LabelEncoder().fit_transform(setup_with_features.feature_six)
-    setup_with_features['feature_seven'] = LabelEncoder().fit_transform(setup_with_features.feature_seven)
-    setup_with_features['feature_eight'] = LabelEncoder().fit_transform(setup_with_features.feature_eight)
-    setup_with_features['feature_nine'] = LabelEncoder().fit_transform(setup_with_features.feature_nine)
-    setup_with_features['feature_ten'] = LabelEncoder().fit_transform(setup_with_features.feature_ten)
 
     featureData['feature_one'] = LabelEncoder().fit_transform(featureData.feature_one)
     featureData['feature_two'] = LabelEncoder().fit_transform(featureData.feature_two)
@@ -71,15 +61,9 @@ else:
 
     feature_test = featureData[int(len(featureData) * .75): int(len(featureData) * 1.0)]
 
-    setup_with_features_train = setup_with_features[
-                                int(len(setup_with_features) * 0.0): int(len(setup_with_features) * .75)]
-    setup_with_features_test = setup_with_features[
-                               int(len(setup_with_features) * .75): int(len(setup_with_features) * 1.0)]
     print(featureData.head())
     print(setupData.head())
     print(laborData.head())
-
-    print(setup_with_features.head())
 
     knn = KNeighborsClassifier(n_neighbors=5, metric='euclidean')
     print("feature_train: ")
@@ -92,18 +76,3 @@ else:
     y_pred = knn.predict(feature_test)
     print(y_pred)
 
-    svr = SVR()
-
-    encoder = OrdinalEncoder()
-    encoder.fit(feature_train)
-    feature_train = encoder.transform(feature_train)
-    encoder.fit(feature_test)
-
-    feature_test = encoder.transform(feature_test)
-
-    svr.fit(feature_train, setup_train.values.ravel())
-
-    svr.fit(feature_train, labor_train.values.ravel())
-
-    #print("Accuracy Setup: {:.2f}".format(svr.score(feature_test, setup_test)))
-    #print("Accuracy Labor: {:.2f}".format(svr.score(feature_test, labor_test)))
